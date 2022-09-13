5304e92aa5c4b304867fa68bb456d89fd48af40a
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-12 20:44:40 -0500

    add keyboard presses to the script

---
+++ src/mousemover/main.py
@@ -0,0 +1,22 @@
+import pyautogui
+from . import constants
+from . import routines
+
+#--------------------------------------------------
+# Main logic
+#--------------------------------------------------
+def main():
+    pyautogui.FAILSAFE = False
+
+    # print program data
+    routines.print_program_data(constants.MAX_CYCLES)
+
+    # make program sleep for specified sleep cycle
+    for _ in range(constants.MAX_CYCLES):
+        routines.move_mouse()
+        routines.change_volume()
+        routines.report_movement()
+
+
+# run the main method
+main()
